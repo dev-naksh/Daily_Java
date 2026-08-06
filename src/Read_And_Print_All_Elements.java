3baec59938aee5b73f28c7740b4893eb70442e61
Nakshatra Chaudhary <nakshatrachaudhary87@gmail.com>
2026-08-07 05:24:38 +0530

    Read and Print All elements in an array in java

---
+++ src/Read_And_Print_All_Elements.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Read_And_Print_All_Elements {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of Array");
+        int n =sc.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter Elements: ");
+        for(int i= 0;i<n;i++) {
+            a[i]= sc.nextInt();
+        }
+
+        System.out.println("elements of array are: ");
+        for(int i =0; i<n;i++){
+            System.out.println(a[i] );
+        }
+         sc.close();
+    }
+}
